46fd5853018795d9386f2319a85c4ec25c957919
Ishanka Senevirathne <ishankadsenevirathne@gmail.com>
2022-12-14 14:52:54 +0530

    removing node server

--- server/src/interfaces/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './student.input.validators';
-export * from './user.input.validators';
-
-export interface Student {
-  id:number,name:string,gender:string,address:string,mobileNo:number,dob:Date,age:number
-}
-
--- server/src/middleware/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './input.validation.guard';
-export * from './admin.auth.guard';
-export * from './local.auth.guard';
-export * from './user.auth.guard';
